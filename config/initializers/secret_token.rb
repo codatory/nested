5427160b58d7ef8285a852566cd9326dbd1201f6
Alex Conner <hello@codatory.com>
2012-05-09 17:27:29 -0400

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nested::Application.config.secret_token = '72e9b0eb1f2b2dcfc344531569313e7786ecfe3549284c7d4073b444701730f13a2e2e200c9e69070801f3db55edca7882a4609ec1ed11ce42c2a3f733983b23'
